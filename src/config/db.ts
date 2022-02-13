65829295434c6e2cbf27d9ffbff5e8869ae00fa2
Sk Samidul Islam <40350468+samidul-tict@users.noreply.github.com>
2022-02-13 10:47:14 -0500

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import morgan from "morgan";
 //* Database connection *//
 export const connection = () => {
-    mongoose.connect('mongodb+srv://aonemart:aonemart_123@aonemart.zk6uk.mongodb.net/aonemart?retryWrites=true&w=majority', 
+    mongoose.connect('mongodb+srv://test.com', 
 {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log('database connected'))
     .catch(err => console.log(err))
